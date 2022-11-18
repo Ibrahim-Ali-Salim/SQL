d345f134ed81782772f19321585fa5f125b036e7
Ibrahim Salim <86985890+Ibrahim-Ali-Salim@users.noreply.github.com>
2022-11-18 08:39:37 -0500

    Add files via upload

---
+++ Total_Purchasing_And_Sales.sql
@@ -0,0 +1,18 @@
+SELECT distinct A.[ProductID]
+      ,A.[Name]
+      ,A.[ProductNumber]
+      ,A.[MakeFlag]
+      ,A.[FinishedGoodsFlag]
+      ,A.[Color]
+      ,A.[SafetyStockLevel]
+      ,A.[ReorderPoint]
+      ,A.[StandardCost]
+      ,A.[ListPrice]
+	  ,TotalSales = SUM((B.OrderQty*B.UnitPrice)-B.UnitPriceDiscount) OVER(PARTITION BY A.ProductID)
+	  ,TotalSales = SUM(C.OrderQty*C.UnitPrice) OVER(PARTITION BY A.ProductID)
+
+  FROM [AdventureWorks2019].[Production].[Product] as A
+	FULL JOIN AdventureWorks2019.Sales.SalesOrderDetail B
+		ON A.ProductID = B.ProductID
+	FULL JOIN  AdventureWorks2019.Purchasing.PurchaseOrderDetail C 
+		ON A.ProductID = C.ProductID
