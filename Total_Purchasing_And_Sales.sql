01626b89c80de00865bef672c2189fbdaf40bdbe
Ibrahim Salim <86985890+Ibrahim-Ali-Salim@users.noreply.github.com>
2022-11-18 08:42:03 -0500

    Update Total_Purchasing_And_Sales.sql

--- Total_Purchasing_And_Sales.sql
+++ Total_Purchasing_And_Sales.sql
@@ -9,8 +9,8 @@ SELECT distinct A.[ProductID]
       ,A.[ReorderPoint]
       ,A.[StandardCost]
       ,A.[ListPrice]
-	  ,TotalSales = SUM((B.OrderQty*B.UnitPrice)-B.UnitPriceDiscount) OVER(PARTITION BY A.ProductID)
-	  ,TotalSales = SUM(C.OrderQty*C.UnitPrice) OVER(PARTITION BY A.ProductID)
+      ,TotalSales = SUM((B.OrderQty*B.UnitPrice)-B.UnitPriceDiscount) OVER(PARTITION BY A.ProductID)
+      ,TotalSales = SUM(C.OrderQty*C.UnitPrice) OVER(PARTITION BY A.ProductID)
 
   FROM [AdventureWorks2019].[Production].[Product] as A
 	FULL JOIN AdventureWorks2019.Sales.SalesOrderDetail B
